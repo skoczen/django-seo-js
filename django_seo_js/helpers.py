94b2ddf0ba0a7730e027fd695b8c6e56a296e343
Brian Wu <brianw@doordash.com>
2016-06-14 15:29:45 -0700

    implement white list

--- django_seo_js/helpers.py
+++ django_seo_js/helpers.py
@@ -1,3 +1,5 @@
+import fnmatch
+import re
 from django_seo_js import settings
 from django_seo_js.backends import SelectedBackend
 
@@ -10,6 +12,13 @@ def update_cache_for_url(url):
 
 
 def request_should_be_ignored(request):
+    if settings.ALLOWED_URLS:
+        # white list urls enabled - only urls in the white list will be pre-rendered
+        if request_in_white_list(request):
+            return False
+
+        return True
+
     for url in settings.IGNORE_URLS:
         if url in request.path:
             return True
@@ -22,3 +31,13 @@ def request_should_be_ignored(request):
 
     extension = request.path[last_dot:]
     return extension and extension in settings.IGNORE_EXTENSIONS
+
+
+def request_in_white_list(request):
+    for pattern in settings.ALLOWED_URLS:
+        regex = fnmatch.translate(pattern)
+        re_obj = re.compile(regex)
+        if re_obj.match(request.path):
+            return True
+
+    return False
